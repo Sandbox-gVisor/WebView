a51e52792c2666647d528a8cb2a13f8dc5b8be94
BigCubeCat <bigcubecat@yandex.ru>
2023-07-28 14:35:44 +0700

    fix(address): address input title

--- src/components/Sidebar/AppBar.tsx
+++ src/components/Sidebar/AppBar.tsx
@@ -1,24 +1,22 @@
 import {
-  AppBar, Box, Typography
+  AppBar, Box,
 } from "@mui/material";
 import AddressInput from "../Address/AddressInput";
 
 export default function HeaderBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="transparent">
-        <Box sx={{ display: "flex", justifyContent: 'space-between', }}>
-          <Box sx={{
-            padding: 3, display: "flex", alignItems: "center",
-          }} >
-            <img src="/logo.png" width="48" height="48" />
-            <div className="Logo" style={{ marginLeft: 20 }}>
-              gWisord
-            </div>
-          </Box>
-          <AddressInput />
+    <AppBar position="static" color="default">
+      <Box sx={{ display: "flex", justifyContent: 'space-between', }}>
+        <Box sx={{
+          padding: 3, display: "flex", alignItems: "center",
+        }} >
+          <img src="/logo.png" width="48" height="48" />
+          <div className="Logo" style={{ marginLeft: 20 }}>
+            gWisord
+          </div>
         </Box>
-      </AppBar>
-    </Box>
+        <AddressInput />
+      </Box>
+    </AppBar>
   );
 }
